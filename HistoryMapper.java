3f2191b8a7b8c7e73255e2028354684f69d1390f
chaochengzhu <70723055+chaochengzhu@users.noreply.github.com>
2023-02-15 18:47:24 -0500

    Delete HistoryMapper.java

--- HistoryMapper.java
+++
@@ -1,8 +0,0 @@
-package mapper;
-
-public interface HistoryMapper {
-
-    int updateHistory(String pid, String fromAct, String toAct, double balance);
-
-    int updateHistory1(String pid,  String fromAct,double balance );
-}
